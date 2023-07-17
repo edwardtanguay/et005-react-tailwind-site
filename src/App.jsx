575140ab994f3247a95106bc0f87dd9f8ee10cf2
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-17 18:13:41 +0200

    tailwind works

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex-col bg-yellow-200 flex justify-center items-center ">
+      <h1 className="text-blue-500 text-9xl">Tailwind Test</h1>
+      <div className="text-4xl m-4 border border-8 border-red-500 p-4 bg-green-600">This is just a test.</div>
+    </div>
+  );
+}
+
+export default App;
